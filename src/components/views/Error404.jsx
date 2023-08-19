512da5fdc0ac2d7eb42f2d585431132dbb58696a
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-19 00:45:34 -0300

    agregue un gif al componente error 404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,5 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
 const Error404 = () => {
-  return <div>Error404</div>;
+  return (
+    <>
+      <div className="container">
+        <img
+          src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExODV4dHoxM2ZzdGxwM2I2cXA4bTU1d2kwaG05NmsyemRkdzhsNzNtNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H54feNXf6i4eAQubud/giphy.gif"
+          alt=""
+        ></img>
+        <h1>La Pagina que ha solicitado no se encuentra disponible</h1>
+        <Button>Volver al Inicio</Button>
+      </div>
+    </>
+  );
 };
 
 export default Error404;
