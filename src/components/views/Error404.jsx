5cc3d0e4013855df1e4f361e8b50f02c6e6c163a
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-20 23:01:38 -0300

    termine maquetado

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -5,9 +5,21 @@ const Error404 = () => {
   return (
     <>
       <div className="container">
-        <img src="https://media.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif"></img>
-        <h3>La Pagina que ha solicitado no se encuentra disponible</h3>
-        <Button variant="danger">Volver al Inicio</Button>
+        <img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXVpMWlybHNvc3ZvN2F2Z2g4NGRidWZnaDlndG9mb3NxYmxjNjEwdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WQOIEQRgiK722l3PQT/giphy.gif"></img>
+        <h3 className="text-white">
+          La Pagina que ha solicitado no se encuentra disponible
+        </h3>
+        <img
+          src="https://assets.codepen.io/1538474/astronaut.svg"
+          class="astronaut"
+        />
+        <img
+          src="https://assets.codepen.io/1538474/spaceship.svg"
+          class="spaceship"
+        />
+        <Button variant="danger" className="mb-5">
+          Volver al Inicio
+        </Button>
       </div>
     </>
   );
