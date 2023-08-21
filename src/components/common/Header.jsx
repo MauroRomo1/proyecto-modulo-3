5bd17b7ef49b15fbfaba21f0d0bd8123a55894f7
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-21 14:22:52 -0300

    creo una variable con propiedades para evaluarsi el usuario esta logeado y mostrar los diferentes componentes

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,8 +1,61 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
 const Header = () => {
+  // const usuario = { nombre: "jose", email: "jose@32", rol: "administrador" };
+  const usuario = null;
   return (
-    <header className="text-center py-2 border-bottom">
-      <h4>navbar de la pagina</h4>
-    </header>
+    <Navbar expand="lg" className=" bg-body-tertiary ">
+      <Container>
+        <Navbar.Brand href="#home">logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="text-center">
+          <Nav className="ms-auto ">
+            <NavLink end to={"/"} className="nav-link">
+              inicio
+            </NavLink>
+            {usuario ? (
+              <>
+                <NavDropdown title="usuario" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">
+                    Mi lista de juegos
+                  </NavDropdown.Item>
+                  {usuario.rol === "administrador" ? (
+                    <NavLink
+                      end
+                      to={"/administracion"}
+                      className="dropdown-item"
+                    >
+                      administracion
+                    </NavLink>
+                  ) : (
+                    <NavDropdown.Item href="#action/3.2">
+                      configuracion
+                    </NavDropdown.Item>
+                  )}
+                  <NavDropdown.Item>
+                    <NavLink end to={"/hsdhs"} className="nav-link">
+                      cerrar sesion
+                    </NavLink>
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            ) : (
+              <>
+                <NavLink end to={"/login"} className="nav-link">
+                  iniciar sesion
+                </NavLink>
+                <NavLink end to={"/ahah"} className="nav-link">
+                  regitrarse
+                </NavLink>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
