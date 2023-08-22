e5ac1db44f995cf39817831fcfbf5b1a3b25f60c
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-08-21 21:13:01 -0300

    Agregando validaciones a email y password

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -2,23 +2,73 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Col, Container, Row, } from "react-bootstrap";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
 
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (usuarioValidado) => {
+    console.log('Los Datos del Formulario cumplen las validaciones.')
+  }
+
   return (
     <>
       <div className="d-flex justify-content-center bg-secondary bgLogin" style={{ minHeight: '100vh', paddingTop: '4vh' }}>
         <Container className="bg-light mb-auto rounded-4 shadow" style={{ maxWidth: '452px', padding: '30px 52px 29px' }}>
+
           <h4 className="fw-bold titulo-page-login">
             Acceder o crear una cuenta
           </h4>
-          <form className="mt-3">
+
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-3">
+
             <FloatingLabel label="Correo electronico" className="mb-3">
-              <Form.Control type="email" placeholder="nombre@ejemplo.com" />
+              <Form.Control
+                type="email"
+                placeholder="nombre@ejemplo.com"
+                {
+                ...register('email', {
+                  required: 'El email es un dato obligatorio',
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: 'El email debe cumplir con un formato valido, ej: nombre@mail.com'
+                  }
+                })
+                }
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+
+            </FloatingLabel>
+            <FloatingLabel label="password" className="mb-3">
+              <Form.Control
+                type="password"
+                placeholder="password"
+                {
+                ...register('password', {
+                  required: 'El password es un dato obligatorio',
+                  pattern: {
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message: 'El password debe contener al menos una letra mayuscula, una minuscula, un numero y entre 8 y 16 caracteres'
+                  }
+                })
+                }
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </FloatingLabel>
             <Button variant="primary" type="submit" style={{ display: 'block', width: '100%' }} className="shadow">
               Continuar
             </Button>
+
             <h5 className="mt-4 mb-3 subtitulo-page-login">
               Acceso rapido con:
             </h5>
@@ -26,19 +76,19 @@ const Login = () => {
             <div className="conteiner px-3 py-1">
               <Row className="gx-3 py-1 gy-2 justify-content-center">
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social border-light-subtle shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/google.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     Google
                   </Button>
                 </Col>
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/facebook.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     Facebook
                   </Button>
                 </Col>
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/paypal.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     PayPal
                   </Button>
@@ -47,19 +97,19 @@ const Login = () => {
 
               <Row className="gx-3 py-1 gy-2 justify-content-center">
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social border-light-subtle shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/tiktok.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     TikTok
                   </Button>
                 </Col>
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social border-light-subtle shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/twitch.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     Twitch
                   </Button>
                 </Col>
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social border-light-subtle shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/twitter.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     Twitter
                   </Button>
@@ -68,13 +118,13 @@ const Login = () => {
 
               <Row className="gx-3 py-1 gy-2 justify-content-center">
                 <Col xs={12} md={4} lg={4} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social border-light-subtle shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/apple.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     Apple
                   </Button>
                 </Col>
                 <Col xs={12} md={8} lg={8} className="text-center px-1">
-                  <Button variant="light" type="submit" className="fuente-boton-social shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
+                  <Button as={Link} to={"/error404"} variant="light" type="submit" className="fuente-boton-social border-light-subtle shadow" style={{ width: '100%', paddingRight: '0', paddingLeft: '0' }}>
                     <img src="/public/telefono.svg" alt="logo google" style={{ paddingRight: '8px' }}></img>
                     Teléfono-sólo vendedores
                   </Button>
