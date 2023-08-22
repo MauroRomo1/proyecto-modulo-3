975a6e0e61d3b37b9cc09eb7f33ac27530a33898
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-21 23:48:15 -0300

    correcciones de errores y agregado .env

--- src/components/views/AcercaDeNosotros.jsx
+++ src/components/views/AcercaDeNosotros.jsx
@@ -1,7 +1,31 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "../../css/acercaDeNosotros.css";
+
 const AcercaDeNosotros = () => {
   return (
     <>
-      <h5>Pagina de acerca de nosotros</h5>
+      <Row xs={1} md={5} className="g-4 p-3">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://i.postimg.cc/0rgthBy1/Sin-t-tulo.jpg"
+              />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
