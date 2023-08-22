c9cb5d96c650a79042815ce4cabed4dfb9af1be1
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-22 00:03:58 -0300

    limpieza de codigo

--- src/components/views/AcercaDeNosotros.jsx
+++ src/components/views/AcercaDeNosotros.jsx
@@ -1,33 +1,5 @@
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import "../../css/acercaDeNosotros.css";
-
 const AcercaDeNosotros = () => {
-  return (
-    <>
-      {/* <Row xs={1} md={5} className="g-4 p-3">
-        {Array.from({ length: 4 }).map((_, idx) => (
-          <Col key={idx}>
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://i.postimg.cc/0rgthBy1/Sin-t-tulo.jpg"
-              />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row> */}
-    </>
-  );
+  return <></>;
 };
 
 export default AcercaDeNosotros;
