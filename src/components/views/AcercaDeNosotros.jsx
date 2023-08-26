100c07384f86d97590621479149fb9f055279143
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-26 00:23:39 -0300

    creacion de la rama y comence maquetado pagina acerca de nosotros

--- src/components/views/AcercaDeNosotros.jsx
+++ src/components/views/AcercaDeNosotros.jsx
@@ -1,5 +1,63 @@
-const AcercaDeNosotros = () => {
-  return <></>;
-};
+import "../../css/acercadenosotros.css";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
-export default AcercaDeNosotros;
+function GroupExample() {
+  return (
+    <>
+      <div className="backg">
+        <CardGroup className="floating support">
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://i.postimg.cc/kGbVStjV/avatar-agustin-jpeg.jpg"
+            />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://i.postimg.cc/NfpMMckq/Whats-App-Image-2023-08-23-at-00-05-09.jpg"
+            />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+      </div>
+    </>
+  );
+}
+
+export default GroupExample;
