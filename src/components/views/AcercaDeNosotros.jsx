a21e958350fe2fa7cd71614ee567e6be7e90ef47
matiasfsd7 <matias.n.ortega@gmail.com>
2023-08-27 19:54:34 -0300

    maquetado y responsive terminado

--- src/components/views/AcercaDeNosotros.jsx
+++ src/components/views/AcercaDeNosotros.jsx
@@ -6,31 +6,87 @@ function GroupExample() {
   return (
     <>
       <div className="backg">
-        <CardGroup className="floating support">
+        <h2 className="text-white text-center"> ¿QUIENES SOMOS?🧐</h2>
+        <div>
+          <h3 className="containerdos text-white text-center">
+            Nuestro empresa cuenta con un equipo de profesionales altamente
+            capacitados en el área de desarrollo web y diseño gráfico. Con una
+            amplia experiencia en la industria quienes han trabajado en una
+            variedad de proyectos web alrededeor del mundo. El equipo está
+            liderado por un scrum master experimentado que asegura que el
+            trabajo se realice de manera eficiente y efectiva. Además, el equipo
+            está formado por desarrolladores front-end y back-end que trabajan
+            juntos para crear una página web dinámica y atractiva para nuestros
+            usuarios.
+          </h3>
+        </div>
+        <CardGroup className="m-3 p-3">
           <Card>
             <Card.Img
               variant="top"
-              src="https://i.postimg.cc/kGbVStjV/avatar-agustin-jpeg.jpg"
+              src="https://i.postimg.cc/nzskmBg6/photo-2016-10-23-23-01-20.jpg"
+              alt="foto o avatar del scrum master"
             />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
+            <Card.Body className="text-center">
+              <Card.Title>Matias Nicolas Ortega</Card.Title>
+              <Card.Text>Student RollingCode School.</Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">Scrum Master</small>
             </Card.Footer>
           </Card>
           <Card>
             <Card.Img
               variant="top"
-              src="https://i.postimg.cc/NfpMMckq/Whats-App-Image-2023-08-23-at-00-05-09.jpg"
+              src="https://i.postimg.cc/PrqqQy2p/se-ores-del-tablero.jpg"
+              alt="Imagen los maestros del tablero"
+            />
+            <Card.Body className="text-center">
+              <Card.Title>Los Maestros del Tablero</Card.Title>
+              <Card.Text>Nuestro Escudo 😁. </Card.Text>
+            </Card.Body>
+            <Card.Footer className=" text-center">
+              <small className="text-muted">The masters of the board </small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://i.postimg.cc/25STzPwj/avatar-agustin-jpeg.jpg"
+              alt="foto o avatar del tech led"
             />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>Mauro Agustin Romo</Card.Title>
+              <Card.Text>Student RollingCode School.</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Tech Led</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+        <CardGroup className="p-3 m-3">
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://i.postimg.cc/13hwCbT7/mauricio-aguierre.jpg"
+              alt="foto o avatar del developer mauricio aguirre"
+            />
+            <Card.Body>
+              <Card.Title>Mauricio Aguirre</Card.Title>
+              <Card.Text>Student RollingCode School.</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Developer</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img
+              variant="top"
+              src="https://i.postimg.cc/2SKTS0X4/Whats-App-Image-2023-08-23-at-00-38-24-1.jpg"
+              alt="foto o avatar del developer nicolas almiron"
+            />
+            <Card.Body>
+              <Card.Title>Nicolas Almiron</Card.Title>
               <Card.Text>
                 This card has supporting text below as a natural lead-in to
                 additional content.{" "}
@@ -41,7 +97,11 @@ function GroupExample() {
             </Card.Footer>
           </Card>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img
+              variant="top"
+              src="https://i.postimg.cc/VLjW8YjP/Jose-rivadeneira.jpg"
+              alt="foto o avatar del developer jose rivadeneira"
+            />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
               <Card.Text>
@@ -61,3 +121,10 @@ function GroupExample() {
 }
 
 export default GroupExample;
+
+//  {
+//    /* <img
+//           src="https://i.postimg.cc/261sTkHM/se-ores-del-tablero.jpg"
+//           alt=""
+//         ></img> */
+// //  }
